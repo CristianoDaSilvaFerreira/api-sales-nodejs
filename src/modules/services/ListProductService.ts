fa2e29a87f065c631a475fa6fc769a222d4dd9cb
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-06-26 09:29:49 -0300

    :up: Serviço para lista todos os produtos

---
+++ src/modules/services/ListProductService.ts
@@ -0,0 +1,16 @@
+import { ProductRepository } from '@modules/produts/typeorm/repositories/ProductRepository';
+import { getCustomRepository } from 'typeorm';
+import Product from '@modules/produts/typeorm/entities/Product';
+
+
+class ListProductService {
+  public async execute(): Promise<Product[]> {
+    const productRepository = getCustomRepository(ProductRepository);
+  
+    const products = productRepository.find();
+
+    return products;
+  }
+}
+
+export default ListProductService;
