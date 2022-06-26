871c0b6564cb110d90ff2f13d73338de0c079bad
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-06-26 17:54:00 -0300

    :up: Refactor Product Service Delete

--- src/modules/products/services/DeleteProductService.ts
+++ src/modules/products/services/DeleteProductService.ts
@@ -1,13 +1,14 @@
 import AppError from '@shared/errors/AppErro';
 import { getCustomRepository } from 'typeorm';
+import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductRepository';
 
-interface IRequest {
+interface IProduct {
   id: string;
 }
 
 class DeleteProductService {
-  public async execute({ id }: IRequest): Promise<void> {
+  public async execute({ id }: IProduct): Promise<Product> {
     const productsRepository = getCustomRepository(ProductRepository);
 
     const product = await productsRepository.findOne(id);
@@ -17,6 +18,8 @@ class DeleteProductService {
     }
 
     await productsRepository.remove(product);
+
+    return product;
   }
 }
 
