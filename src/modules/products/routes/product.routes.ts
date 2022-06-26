7407a65f266660a8df3e796b61da0b50a7f55ac6
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-06-26 12:51:11 -0300

    :rocket: Validação dos dados da requisição

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -1,16 +1,61 @@
 import { Router } from 'express';
 import ProductsController from '../controller/ProductService';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const productsRouter = Router();
 const productsController = new ProductsController();
 
 productsRouter.get('/', productsController.indexProduct);
-productsRouter.get('/:id', productsController.showProduct);
 
-productsRouter.post('/', productsController.createProduct);
+productsRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  productsController.showProduct,
+);
 
-productsRouter.put('/:id', productsController.updateProduct);
+productsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+  }),
 
-productsRouter.delete('/:id', productsController.deleteProduct);
+  productsController.createProduct,
+);
+
+productsRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().precision(2).required(),
+      quantity: Joi.number().required(),
+    },
+
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+
+  productsController.updateProduct,
+);
+
+productsRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+
+  productsController.deleteProduct,
+);
 
 export default productsRouter;
