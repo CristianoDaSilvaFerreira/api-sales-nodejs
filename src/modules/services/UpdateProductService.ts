4e633904d55022f575bfada5cd0146510b511e46
CristianoDaSilvaFerreira <cristianodevsystem@gmail.com>
2022-06-26 09:41:40 -0300

    :up: Serviço para atualizar o produto

--- src/modules/services/UpdateProductService.ts
+++ src/modules/services/UpdateProductService.ts
@@ -5,22 +5,38 @@ import Product from '@modules/produts/typeorm/entities/Product';
 
 interface IProduct {
   id: string;
+  name: string;
+  price: number;
+  quantity: number;
 }
 
-class ShowProductService {
-  public async execute({ id }: IProduct): Promise<Product> {
+class UpdateProductService {
+  public async execute({ id, name, price, quantity }: IProduct): Promise<Product> {
     const productRepository = getCustomRepository(ProductRepository);
 
     const product = await productRepository.findOne({
       where: { id: id },
     });
 
+    
     if (!product) {
       throw new AppError('Product not found!');
     }
+    
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists && name !== product.name) {
+      throw new AppError('There is already a product with this name');
+    }
+
+    product.name = name;
+    product.price = price;
+    product.quantity = quantity;
+
+    await productRepository.save(product);
 
     return product;
   }
 }
 
-export default ShowProductService;
+export default UpdateProductService;
